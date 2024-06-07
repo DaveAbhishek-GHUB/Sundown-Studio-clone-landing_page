6bf706aaec1d857e1dd8f852b599ef3008054241
Dave Abhishek <abhishekjdave0309@gmail.com>
2024-06-07 12:08:32 +0530

    make page1 responsive

--- script.js
+++ script.js
@@ -96,3 +96,21 @@ function swiperAnimation() {}
 
 swiperAnimation();
 page04Animaiton();
+
+var menu = document.querySelector("nav h3");
+var full = document.querySelector("#full-scr");
+var navimg = document.querySelector("nav img")
+var flag = 0
+
+menu.addEventListener("click",function(){
+  if(flag == 0){
+    full.style.top = 0;
+    navimg.style.opacity = 0
+    flag = 1
+  }
+  else{
+    full.style.top = "-100%"
+    navimg.style.opacity = 1
+    flag = 0
+  }
+})
